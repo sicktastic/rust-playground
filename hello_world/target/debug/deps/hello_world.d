f68ef380322ba257c96ede785337f1135305b826
Anthony Lee <antwonlee@gmail.com>
2021-11-08 15:51:38 -0600

    wip: Remove target directory

--- hello_world/target/debug/deps/hello_world.d
+++
@@ -1,5 +0,0 @@
-/Users/anthonylee/Sites/sicktastic/rust-playground/hello_world/target/debug/deps/hello_world: src/main.rs
-
-/Users/anthonylee/Sites/sicktastic/rust-playground/hello_world/target/debug/deps/hello_world.d: src/main.rs
-
-src/main.rs:
--- hello_world/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-/Users/anthonylee/Sites/sicktastic/rust-playground/hello_world/target/debug/hello_world: /Users/anthonylee/Sites/sicktastic/rust-playground/hello_world/src/main.rs
