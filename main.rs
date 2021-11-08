bd7d2828c60afc98f9ca61992e0ffe31ada573c8
Anthony Lee <antwonlee@gmail.com>
2021-11-08 15:31:56 -0600

    wip: Adding cargo project

--- main.rs
+++ main.rs
@@ -1,19 +1,25 @@
 fn main() {
-    let logical: bool = true;
+    // let logical: bool = true;
 
-    let a_float: f64 = 1.0;
-    let an_integer = 5i32; // Suffix annotation
+    // let a_float: f64 = 1.0;
+    // let an_integer = 5i32;
 
-    let default_float = 3.0;
-    let default_integer = 7;
+    // let default_float = 3.0;
+    // let default_integer = 7;
 
-    let mut inferred_type  = 12; // Type i64 is inferred from another line
-    inferred_type = 4294967296i64;
+    // let mut inferred_type  = 12;
+    // inferred_type = 4294967296i64;
 
-    let mut mutable = 12; // Mutable `i32`
-    mutable = 21;
+    // let mut mutable = 12;
+    // mutable = 21;
 
-    mutable = true;
+    // mutable = true;
 
-    let mutable = true;
+    // let mutable = true;
+
+    println!("1 + 2 = {}", 1u32 + 2);
+
+    println!("1 -2  = {}", 1i32 - 2);
+
+    println!("true AND false is {}", true && false);
 }
