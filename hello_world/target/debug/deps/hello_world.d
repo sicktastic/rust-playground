31c854c885fb77d8730567cbb276190a062580d0
Anthony Lee <antwonlee@gmail.com>
2021-11-08 15:44:17 -0600

    wip: Running cargo

---
+++ hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+/Users/anthonylee/Sites/sicktastic/rust-playground/hello_world/target/debug/deps/hello_world: src/main.rs
+
+/Users/anthonylee/Sites/sicktastic/rust-playground/hello_world/target/debug/deps/hello_world.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/anthonylee/Sites/sicktastic/rust-playground/hello_world/target/debug/hello_world: /Users/anthonylee/Sites/sicktastic/rust-playground/hello_world/src/main.rs
